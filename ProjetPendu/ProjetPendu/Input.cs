562b36dadaa0a630e65092cec1d56f226fc4ee51
DamDam00 <dam.jacquet@gmail.com>
2015-04-13 14:43:29 +0200

    Ajout de la classe input et de son interface IInput

---
+++ ProjetPendu/ProjetPendu/Input.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjetPendu
+{
+    public class InputConsole : IInput
+    {
+
+        string Input()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
