54aee95a2b83f138e9e239d2835bd93124ba95bc
DamDam00 <dam.jacquet@gmail.com>
2015-04-13 15:42:31 +0200

    Ajout de la fonction getCurrentState (alors

--- ProjetPendu/ProjetPendu/IOutput.cs
+++ ProjetPendu/ProjetPendu/IOutput.cs
@@ -12,18 +12,18 @@ namespace ProjetPendu
         /// Show the rules of the game
         /// </summary>
         /// <param name="r"></param>
-        public void ShowRules(Rules r);
+        void ShowRules(Rules r);
 
         /// <summary>
         /// Show the character
         /// </summary>
         /// <param name="c"></param>
-        public void ShowCharacter(Character c);
+        void ShowCharacter(Character c);
 
         /// <summary>
         /// Show the word to find with the hidden letters
         /// </summary>
         /// <param name="w"></param>
-        public void ShowWord(Word w);
+        void ShowWord(Word w);
     }
 }
