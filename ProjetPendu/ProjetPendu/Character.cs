7151140ed01b62deeb08666456ae3d0ea25fd526
Gyt94 <gygy.tho@gmail.com>
2015-04-13 14:54:41 +0200

    Creation Ioutput et maj

---
+++ ProjetPendu/ProjetPendu/Character.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjetPendu
+{
+    class Character : ICharacter
+    {
+        private string filepath;
+
+        public void SetFichier(string fileName)
+        {
+            if (fileName.Length > 0)
+                filepath = fileName;
+        }
+
+        public void GetEtatInitial()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetEtatSuivant()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsFinal()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProjetPendu/ProjetPendu/IOutput.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Pendu;
+
+namespace ProjetPendu
+{
+    interface IOutput
+    {
+        /// <summary>
+        /// Show the rules of the game
+        /// </summary>
+        /// <param name="r"></param>
+        public void ShowRules(Rules r);
+
+        /// <summary>
+        /// Show the character
+        /// </summary>
+        /// <param name="c"></param>
+        public void ShowCharacter(Character c);
+
+        /// <summary>
+        /// Show the word to find with the hidden letters
+        /// </summary>
+        /// <param name="w"></param>
+        public void ShowWord(Word w);
+    }
+}
